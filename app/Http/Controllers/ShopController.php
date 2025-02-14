d14f9eb747f607b680c9a30517405d63fd4d873e
JaydipDabhi <jaydipdabhi01@gmail.com>
2025-02-14 12:22:38 +0530

    Create new Shop page and edit ShopController

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    // public function index(Request $request)
-    // {
-    //     $products = Product::orderBy('created_at', 'DESC');
-    //     return view('shop', compact('products'));
-    // }
+    public function index(Request $request)
+    {
+        $products = Product::orderBy('created_at', 'DESC')->paginate(10);
+        return view('shop', compact('products'));
+    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -273,7 +273,7 @@
             </a>
 
             <div class="logo">
-                <a href="index.html">
+                <a href="{{ route('home.index') }}">
                     <img src="{{ asset('assets/images/logo.png') }}" alt="Uomo" class="logo__image d-block" />
                 </a>
             </div>
@@ -313,10 +313,10 @@
                 <div class="overflow-hidden">
                     <ul class="navigation__list list-unstyled position-relative">
                         <li class="navigation__item">
-                            <a href="index.html" class="navigation__link">Home</a>
+                            <a href="{{ route('home.index') }}" class="navigation__link">Home</a>
                         </li>
                         <li class="navigation__item">
-                            <a href="shop.html" class="navigation__link">Shop</a>
+                            <a href="{{ route('shop.index') }}" class="navigation__link">Shop</a>
                         </li>
                         <li class="navigation__item">
                             <a href="cart.html" class="navigation__link">Cart</a>
@@ -406,7 +406,7 @@
                             <a href="{{ route('home.index') }}" class="navigation__link">Home</a>
                         </li>
                         <li class="navigation__item">
-                            <a href="shop.html" class="navigation__link">Shop</a>
+                            <a href="{{ route('shop.index') }}" class="navigation__link">Shop</a>
                         </li>
                         <li class="navigation__item">
                             <a href="cart.html" class="navigation__link">Cart</a>
@@ -521,7 +521,7 @@
             <div class="row row-cols-lg-5 row-cols-2">
                 <div class="footer-column footer-store-info col-12 mb-4 mb-lg-0">
                     <div class="logo">
-                        <a href="index.html">
+                        <a href="{{ route('home.index') }}">
                             <img src="{{ asset('assets/images/logo.png') }}" alt="SurfsideMedia"
                                 class="logo__image d-block" />
                         </a>
@@ -659,7 +659,8 @@
     <footer class="footer-mobile container w-100 px-5 d-md-none bg-body">
         <div class="row text-center">
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('home.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_home" />
@@ -669,7 +670,8 @@
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('home.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_hanger" />
@@ -679,7 +681,8 @@
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('home.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <div class="position-relative">
                         <svg class="d-block" width="18" height="18" viewBox="0 0 20 20" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
