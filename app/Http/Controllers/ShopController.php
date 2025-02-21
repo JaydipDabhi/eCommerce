4e71eea9870e8cbce044512ca5483a416c9d8b18
Jaydip Dabhi <jaydipdabhi01@gmail.com>
2025-02-21 12:53:39 +0530

    filter by brand and category

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -16,6 +18,8 @@ class ShopController extends Controller
         $o_column = "";
         $o_order = "";
         $order = $request->query('order', -1);
+        $f_brands = $request->query('brands', '');
+        $f_categories = $request->query('categories', '');
         switch ($order) {
             case 1:
                 $o_column = 'created_at';
@@ -38,8 +42,14 @@ class ShopController extends Controller
                 $o_column = 'id';
                 $o_order = 'DESC';
         }
-        $products = Product::orderBy($o_column, $o_order)->paginate($size);
-        return view('shop', compact('products', 'size', 'order'));
+        $brands = Brand::orderBy('name', 'ASC')->get();
+        $categories = Category::orderBy('name', 'ASC')->get();
+        $products = Product::where(function ($query) use ($f_brands) {
+            $query->whereIn('brand_id', explode(',', $f_brands))->orWhereRaw("'" . $f_brands . "'=''");
+        })->where(function ($query) use ($f_categories) {
+            $query->whereIn('category_id', explode(',', $f_categories))->orWhereRaw("'" . $f_categories . "'=''");
+        })->orderBy($o_column, $o_order)->paginate($size);
+        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories'));
     }
 
     public function product_details($product_slug)
